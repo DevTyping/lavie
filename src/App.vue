0566c6d4e2038d82399f7de5d3de071991a2d89c
Dat Nguyen <kcboi95@gmail.com>
2020-11-01 18:10:35 +0700

    Update button component & remove babel

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <LaButton />
+    <LaButton :label="'Primary'" kind="primary"/> |
+    <LaButton :label="'Primary field'" kind="primary" size="field"/>  |
+    <LaButton :label="'Primary small'" kind="primary" size="small"/>
+    <br/><br/>
+    <LaButton :label="'Secondary'" kind="secondary"/> |
+    <LaButton :label="'Secondary field'" kind="secondary" size="field"/> |
+    <LaButton :label="'Secondary small'" kind="secondary" size="small"/>
+    <br/><br/>
+    <LaButton :label="'Tertiary'" kind="tertiary"/> |
+    <LaButton :label="'Tertiary field'" kind="tertiary" size="field"/> |
+    <LaButton :label="'Tertiary small'" kind="tertiary" size="small"/>
   </div>
 </template>
 
--- src/components/LaButton/index.vue
+++ src/components/LaButton/index.vue
@@ -1,9 +1,112 @@
 <template>
-    <button class="inline-block border border-gray-400 bg-gray-400 rounded p-1 pr-4 pl-4">
-        Button
-    </button>
+  <button class="inline-block btn btn-default pr-4 pl-4" :class="btnClassOpts">
+    {{ label }}
+  </button>
 </template>
 
-<style scoped src="./styles.css"></style>
+<script>
+export default {
+  name: 'LaButton',
+  props: {
+    label: {
+      type: String,
+      default: 'Button'
+    },
+    kind: {
+      type: String,
+      default: 'primary',
+      validator: val => ['primary', 'secondary', 'tertiary', 'ghost', 'danger', 'danger-primary'].includes(val)
+    },
+    size: { type: String, default: undefined, validator: val => ['', 'field', 'small'].includes(val) },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    btnClassOpts () {
+      let classes = []
+      if (this.primary === true) {
+        classes.push('btn-primary')
+      }
 
-<script src='./script.js'></script>
+      if (this.kind) {
+        classes.push(`btn-${this.kind.toLowerCase()}`)
+      }
+
+      if (this.size === 'small' || (this.size === undefined && this.small)) {
+        classes.push('btn-small')
+      }
+      if (this.size === 'field') {
+        classes.push('btn-field')
+      }
+
+      if (this.disabled) {
+        classes.push('btn-disabled')
+      }
+      return classes.join(' ')
+    }
+  }
+}
+</script>
+
+<style>
+@import "../../css/lavie";
+
+.btn-default {
+  height: 48px;
+}
+
+.btn-field {
+  height: 40px;
+}
+
+.btn-small {
+  height: 32px;
+}
+
+.btn-primary {
+  background: var(--interactive-01);
+  color: var(--text-04);
+  border: 1px solid var(--interactive-01);
+}
+
+.btn-primary:hover {
+  background: var(--hover-primary);
+  border: 1px solid var(--hover-primary);
+}
+
+.btn-secondary {
+  background: var(--interactive-02);
+  color: var(--text-04);
+  border: 1px solid var(--interactive-02);
+}
+
+.btn-secondary:hover {
+  background: var(--hover-secondary);
+  border: 1px solid var(--hover-secondary);
+}
+
+.btn-tertiary {
+  background: var(--inverse-01);
+  color: var(--interactive-01);
+  border: 1px solid var(--interactive-01);
+}
+
+.btn-tertiary:hover {
+  background: var(--hover-tertiary);
+  color: var(--text-04);
+  border: 1px solid var(--hover-tertiary);
+}
+
+.btn-danger {
+  background: var(--danger);
+  color: var(--text-04);
+  border: 1px solid var(--danger);
+}
+
+.btn-danger:hover {
+  background: var(--hover-danger);
+  border: 1px solid var(--hover-danger);
+}
+</style>
